101241ffeb810d76dcd9f300e472110bd4157ec3
salvatore sorrentino <salvatore.sorrentino@live.com>
2024-07-04 15:48:05 +0200

    Componente Mappa

---
+++ BlazorDemoComponentsLibrary/Interop/MapMarker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorDemoComponentsLibrary.Interop;
+
+public class MapMarker
+{
+    public double Lat { get; set; }
+    public double Lng { get; set; }
+    public string PopupContent { get; set; } = "Title";
+}
